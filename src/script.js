96246c1ac13865e26c75b8b1bbe3e4c5456cd03a
Vitor <vitorjvbm@gmail.com>
2025-03-15 02:03:12 -0300

    Type functions add

--- src/script.js
+++ src/script.js
@@ -1,2 +1,10 @@
-"use strict";
-let numero = 7;
+function multiplica(a, b) {
+    return a * b;
+}
+function ola(nome) {
+    return "Olá " + nome;
+}
+// testss
+console.log(multiplica(7, 7));
+console.log(ola("Vitor"));
+console.log(ola("José Vitor"));
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,12 @@
-let numero = 7;
+function multiplica(a: number, b: number): number {
+	return a * b;
+}
+
+function ola(nome: string): string {
+	return "Olá " + nome;
+}
+
+// testss
+console.log(multiplica(7, 7));
+console.log(ola("Vitor"));
+console.log(ola("José Vitor"));
